fa1eb68e07938cf9a9694f406d9b1a2a87ed2687
kabdala <73123847+kabdala@users.noreply.github.com>
2020-10-31 23:11:35 +0500

    student class
    in this class the codes and comments are updated according to the requirements

--- app03-studentGrades/Student.java
+++ app03-studentGrades/Student.java
@@ -1,94 +1,93 @@
 import java.util.*;
 
 /**
- * The Student class represents a student in a student administration system.
- * It holds the student details relevant in our context.
- * 
- * @author Michael Kölling and David Barnes
- * @version 2016.02.29
+ * This class is gonig to carry on student data and information
+ * Edited: Khayre Abdala
+ * Version 31/10/2020
  */
 public class Student
 {
-    // the student's full name
-    private String name;
-    // the student ID
-    private String id;
-    // the amount of credits for study taken so far
+    // here the student name and student ID is defined in the form of attributes
+    private String nameofstudent;
+
+    private String idofstudent;
+    
     private int credits;
     
     private Course course;
-    
+       
+
     /**
-     * Create a new student with a given name of student and ID number.
+     * here, the student name, student ID and credits details are created
      */
-    public Student(String fullName, String studentID)
+    public Student(String completeName, String sID)
     {
-        name = fullName;
-        id = studentID;
+        nameofstudent = completeName;
+        idofstudent = sID;
         credits = 0;
     }
-
-    /**
-     * Return the full name of this student.
-     */
-    public String getName()
-    {
-        return name;
-    }
-
-    /**
-     * Set a new name for this student.
-     */
-    public void changeName(String replacementName)
-    {
-        name = replacementName;
-    }
-
-    /**
-     * Return the student ID of this student.
-     */
-    public String getStudentID()
-    {
-        return id;
-    }
-
-    /**
-     * Add some credit points to the student's accumulated credits.
-     */
-    public void addCredits(int additionalPoints)
-    {
-        credits += additionalPoints;
-    }
-
     
     /**
-     * Return the number of credit points this student has accumulated.
+     * a particular course name is allocated to the new course added
+     */
+    public void addCourse(Course coursename)
+    {
+        this.course = coursename;
+    }
+    
+    /**
+     * This will return the full or complete student name
+     */
+    public String getcompleteName()
+    {
+        return nameofstudent;
+    }
+
+    /**
+     * here we can gave a new name to the student or we can change the student name
+     */
+    public void changeName(String alterName)
+    {
+        nameofstudent = alterName;
+    }
+
+    /**
+     * To get or to return the ID of the student (sID)
+     */
+    public String getsID()
+    {
+        return idofstudent;
+    }
+
+    /**
+     * here, the extra points or extra marks can given to credits
+     */
+    public void addCredits(int extraMarks)
+    {
+        credits += extraMarks;
+    }
+
+    /**
+     * to get total number of credets, we use following block of code
      */
     public int getCredits()
     {
         return credits;
     }
 
-    /**
-     * Return the login name of this student. The login name is a combination
-     * of the first four characters of the student's name and the first three
-     * characters of the student's ID number.
-     */
+    
     public String getLoginName()
     {
-        return name.substring(0,4) + id.substring(0,3);
+        return nameofstudent.substring(0,4) + idofstudent.substring(0,3);
     }
-    
+
     /**
-     * Print the student's name and ID number to the output terminal.
+     * Following code will print out the student name, student ID and total credits
      */
     public void print()
     {
-        System.out.println(name + ", student ID: " + id + ", credits: " + credits);
-    }
-    
-    public void enrolOnCourse(Course course)
-    {
-        this.course = course;
+        System.out.println(nameofstudent + ", sID: " + idofstudent + ", credits: " + credits);
+        course.print();
     }
 }
+
