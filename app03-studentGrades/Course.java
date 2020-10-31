c3b132a9672b069c3e6d245ad8d8537621e4d151
kabdala <73123847+kabdala@users.noreply.github.com>
2020-10-31 23:06:56 +0500

    Course class
    this class is updated by adding new studetns etc

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -1,35 +1,186 @@
 
 /**
- * This class stores information about a course
- * that enrolled students may want to complete
+ * This class wil handle all workings of course data
  *
- * @author Derek Peacock
- * @version 0.1 11/Sep/2020
+ * @author Khayre Abdala
+ * @version 31/10/2020
  */
 public class Course
 {
-    // instance variables - replace the example below with your own
-    private String codeNo;
-    private String title;
+    // for tile of the courese and for ID of the course
+    private String titleofcourse;
+    private String IDofcourse;
     
+    // four modules are initiated here
+    private Module modulefirst;
+    private Module modulesecond;
+    private Module modulethird;
+    private Module moduleforth;
+    
+    // and integer is defined for the number of the module
+    
+    private int numberofModule;
+    
+    
+
+    // for each module the marks will be saved in these variables
+    private int modulefirstMarks;
+    private int modulesecondMarks;
+    private int modulethirdMarks;
+    private int moduleforthMarks;
+    
+    //the total obtained marks and total grades will be saved in these variables
+    private int obtainedMarks;
+    private String obtainedGrades;
+
     /**
-     * Constructor for objects of class Course
+     * The ID of the course and title of the course are defined 
      */
-    public Course(String codeNo, String title)
+    public Course(String titleofcourse, String ID)
     {
-        // initialise instance variables
-        this.codeNo = codeNo;
-        this.title = title;
+        this.titleofcourse = titleofcourse;
+        IDofcourse = ID;
+        numberofModule = 0;
     }
 
-   
     /**
-     * Prints out the details of a course with code number and title
+     * in this method the four modules are going to be added here
+     */
+    public void addmodule(String Nameofmodule, String Codeofmodule)
+    {
+        numberofModule ++;
+        if (numberofModule == 1)
+        {
+            modulefirst = new Module(Nameofmodule, Codeofmodule);
+        }
+        else if (numberofModule == 2)
+        {
+            modulesecond = new Module(Nameofmodule, Codeofmodule);
+        }
+        else if (numberofModule == 3)
+        {
+            modulethird = new Module(Nameofmodule, Codeofmodule);
+        }
+        else if (numberofModule == 4)
+        {
+            moduleforth = new Module(Nameofmodule, Codeofmodule);
+        }
+        else if (numberofModule == 5)
+        {
+            System.out.println ("This Course is at maximum modules");
+        }
+    }
+
+    /**
+     * from this method the marks can be changed
+     */
+    public void changeMark(String Nameofmodule, int mark)
+    {
+        if (Nameofmodule == modulefirst.getName())
+        {
+            modulefirst.changeMark(mark);
+            modulefirstMarks = mark;
+        }
+        else if (Nameofmodule == modulesecond.getName())
+        {
+            modulesecond.changeMark(mark);
+            modulesecondMarks = mark;
+        }
+        else if (Nameofmodule == modulethird.getName())
+        {
+            modulethird.changeMark(mark);
+            modulethirdMarks = mark;
+        }
+        else if (Nameofmodule == moduleforth.getName())
+        {
+            moduleforth.changeMark(mark);
+            moduleforthMarks = mark;
+        }
+        setGrade();
+    }
+
+    /**
+     * this method will find out the obtained marks and then accordingl the grades will be allocated
+     */
+    public void setGrade()
+    {
+        obtainedMarks = ((modulefirstMarks + modulesecondMarks + modulethirdMarks + moduleforthMarks) / 4);
+        if (obtainedMarks <= 40)
+        {
+            obtainedGrades = "F";
+        }
+        else if ((obtainedMarks <= 49) && (obtainedMarks >= 41))
+        {
+            obtainedGrades = "D";
+        }
+        else if ((obtainedMarks <= 59) && (obtainedMarks >= 50))
+        {
+            obtainedGrades = "C";
+        }
+        else if ((obtainedMarks <= 69) && (obtainedMarks >= 60))
+        {
+            obtainedGrades = "B";
+        }
+        else if ((obtainedMarks <= 100) && (obtainedMarks >= 70))
+        {
+            obtainedGrades = "A";
+        }
+    }
+    
+    /**
+     * here the modules information can be printed
+     */
+    public void printModualDetails(String Nameofmodule)
+    {
+        if (Nameofmodule == modulefirst.getName())
+        {
+            
+            modulefirst.print();
+        }
+        else if (Nameofmodule == modulesecond.getName())
+        {
+                        modulesecond.print();
+        }
+        else if (Nameofmodule == modulethird.getName())
+        {
+                        modulethird.print();
+        }
+        else if (Nameofmodule == moduleforth.getName())
+        {
+                        moduleforth.print();
+        }
+    }
+    
+    /**
+     * from here, the dtails are going to be printed for course and for module
      */
     public void print()
     {
-        // put your code here
-        System.out.println("Course " + codeNo + " - " + title);
+        
+        if (numberofModule <= 3)
+        {
+            System.out.println("A Course needs 4 moduals");
+            System.out.println("Current number of modules: " + numberofModule);
+        }
+      
+        else if (modulefirst.getfinish() == false || modulesecond.getfinish() == false || modulethird.getfinish() == false || modulethird.getfinish() == false)
+        {
+            System.out.println("Some modules are incomplete:");
+            modulefirst.printIfnotfinish();
+            modulesecond.printIfnotfinish();
+            modulethird.printIfnotfinish();
+            moduleforth.printIfnotfinish();
+            
+        }
+        
+        else
+        { 
+            System.out.println ("Course: " + titleofcourse + ", IDofcourse: " + IDofcourse);
+            modulefirst.print();
+            modulesecond.print();
+            modulethird.print();
+            moduleforth.print();
+            System.out.println ("Your result in the form of grades is: " + obtainedGrades);
+        }
     }
-    
 }
