0fcb005b7974f5c9e99f4fb80549ab8e29eb4891
kabdala <73123847+kabdala@users.noreply.github.com>
2020-11-08 07:12:05 +0500

    author detials
    enter the version number and author details

--- app04-products/Product.java
+++ app04-products/Product.java
@@ -1,8 +1,8 @@
 /**
  * Model some details of a product sold by a company.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Khayre abdala
+ * @version number 1.0
  */
 public class Product
 {
