a8084dba77a719842151373dd2337ea2501ac6be
kabdala <73123847+kabdala@users.noreply.github.com>
2020-11-08 07:12:47 +0500

    Updated stock demo
    new methods are added with complete working of demo

--- app04-products/StockDemo.java
+++ app04-products/StockDemo.java
@@ -3,8 +3,8 @@
  * The demonstration becomes properly functional as
  * the StockManager class is completed.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Khayre Abdala
+ * @version 3.0
  */
 public class StockDemo
 {
@@ -29,11 +29,14 @@ public class StockDemo
         manager.addProduct(new Product(217,  " Toyota Fit"));
         manager.addProduct(new Product(218,  " Mercdeez benz"));
         manager.addProduct(new Product(119,  " Honda Atlas"));
-        manager.addProduct(new Product(220,  " Nissan 2020"));
-	  manager.addProduct(new Product(221,  " Mercdeez benz"));
-        manager.addProduct(new Product(222,  " Honda 2020"));
-        manager.addProduct(new Product(223,  " Nissan 2018"));
+	  
     }
+public void runDemo()
+    {
+       manager.printAllProducts();
+       demoDeliverProducts();
+    }
+
     
     /**
      * Provide a very simple demonstration of how a StockManager
@@ -42,66 +45,31 @@ public class StockDemo
      */
 
 
-    public void demoDeliverProduct()
+    private void demoDeliverProducts()
     {
         // Show details of all of the products before delivery.
-        
-	manager.printProduct(210);
-	manager.printProduct(211);
-	manager.printProduct(212);
-	manager.printProduct(213);
-	manager.printProduct(214);
-	manager.printProduct(215);
-	manager.printProduct(216);
-	manager.printProduct(217);
-	manager.printProduct(218);
-	manager.printProduct(219);
-	manager.printProduct(220);
-	manager.printProduct(221);
-	manager.printProduct(222);
-	manager.printProduct(223);
-        
+     manager.printAllProducts();
+      
         // Take delivery of 5 items of one of the products.
         
-	   manager.delivery(210, 9);
-        manager.delivery(211, 12);
-        manager.delivery(212, 6);
-        manager.delivery(213, 8);
-        manager.delivery(214, 11);
-        manager.delivery(215, 7);
-        manager.delivery(216, 2);
-        manager.delivery(217, 9);
-        manager.delivery(218, 12);
-        manager.delivery(219, 8);
-	   manager.delivery(220, 6);
-        manager.delivery(221, 5);
-        manager.delivery(222, 19);
-        manager.delivery(223, 21);
-
-        
+	   manager.deliverProduct(210, 9);
+        manager.deliverProduct(211, 12);
+        manager.deliverProduct(212, 6);
+        manager.deliverProduct(213, 8);
+        manager.deliverProduct(214, 11);
+        manager.deliverProduct(215, 7);
+        manager.deliverProduct(216, 2);
+        manager.deliverProduct(217, 9);
+        manager.deliverProduct(218, 12);
+        manager.deliverProduct(219, 8);
+	           
         // Show the list of all products after delivery
 
         manager.printAllProducts();
 
     }
     
-    /**
-     * Sell one of the given item.
-     * Show the before and after status of the product.
-     * @param id The ID of the product being sold.
-     */
-    public void demoSellProduct(int id)
-    {
-        Product product = getProduct(id);
         
-        if(product != null) 
-        {
-            manager.printProduct(id);
-            product.sellOne();
-            manager.printProduct(id);
-        }
-    }
-    
     /**
      * Get the product with the given id from the manager.
      * An error message is printed if there is no match.
